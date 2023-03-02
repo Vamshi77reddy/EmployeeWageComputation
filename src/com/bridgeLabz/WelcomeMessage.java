e77b5869ebd1eba4ee8f08d838454987e7baeeed
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-02 18:24:42 +0530

    SwitchCase

--- src/com/bridgeLabz/WelcomeMessage.java
+++ src/com/bridgeLabz/WelcomeMessage.java
@@ -11,14 +11,16 @@ public class WelcomeMessage {
 		        int partTimeHour = 4;
 		        int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		        if (employeeCheck == 1) {
+		        switch (employeeCheck) {
+		        case 1:
 		            dailyWage = fullDayHour * wagePerHour;
 		            System.out.println("Employee is present Full Time");
-		        } else if(employeeCheck == 2){
+		            break;
+		        case 2:
 		        	 dailyWage = partTimeHour * wagePerHour;
 		             System.out.println("Employee is present half time");
-		        }else {
-		            System.out.println("Employee is absent");
+		             break;
+default:		            System.out.println("Employee is absent");
 		        }
 		        System.out.println("Employee total wage is: " + dailyWage);
 		    }
