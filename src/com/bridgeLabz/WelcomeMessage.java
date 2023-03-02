fd2cdb5d30533b6a0d84d062725ec5d3fe15d42c
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-02 18:11:50 +0530

    Added PartTimeWage

--- src/com/bridgeLabz/WelcomeMessage.java
+++ src/com/bridgeLabz/WelcomeMessage.java
@@ -8,12 +8,16 @@ public class WelcomeMessage {
 		        int wagePerHour = 20;
 		        int fullDayHour = 8;
 		        int dailyWage = 0;
-		        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+		        int partTimeHour = 4;
+		        int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 		        if (employeeCheck == 1) {
 		            dailyWage = fullDayHour * wagePerHour;
-		            System.out.println("Employee is present");
-		        } else {
+		            System.out.println("Employee is present Full Time");
+		        } else if(employeeCheck == 2){
+		        	 dailyWage = partTimeHour * wagePerHour;
+		             System.out.println("Employee is present half time");
+		        }else {
 		            System.out.println("Employee is absent");
 		        }
 		        System.out.println("Employee total wage is: " + dailyWage);
