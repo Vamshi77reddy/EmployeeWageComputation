01ff96c63df6275f4d2b2349a36ee63e61c48e26
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-10 10:48:59 +0530

    Added MultiCompanies

--- src/com/bridgeLabz/WelcomeMessage.java
+++ src/com/bridgeLabz/WelcomeMessage.java
@@ -4,41 +4,40 @@ public class WelcomeMessage {
 	
 static final int FullTIme=1;
 static final int PartTime=2;
-static final int WAGEPerHour=20;
-static final int WorkingDaysPerMonth=20;
-static final int TotalWorkingHours=100;
 
- static void calculateWage() {
-int wagePerHour = 20;
+
+ static void calculateWage(String company, int WAGEPerHour, int WorkingDaysPerMonth, int TotalWorkingHours  ) {
 		        int fullDayHour = 8;
 		        int dailyWage = 0;
 		        int partTimeHour = 4;
 		        int totalWorkingDays = 0;
 		        int totalHour=0;
-		        while (totalHour < 80 && totalWorkingDays < 20) {
+		        while (totalHour < TotalWorkingHours && totalWorkingDays < WorkingDaysPerMonth) {
 
 		        int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
 		        totalWorkingDays++;
 		        switch (employeeCheck) {
-		        case 1:
-                    totalHour = totalHour + fullDayHour;
+		        case 1:                    totalHour = totalHour + fullDayHour;
 
-		            dailyWage = dailyWage+fullDayHour * wagePerHour;
-		            System.out.println("Employee is present Full Time" +dailyWage);
+		            dailyWage = dailyWage+fullDayHour * WAGEPerHour;
+		            System.out.println("Employee is present Full Time :" +dailyWage);
 		            break;
 		        case 2:
                     totalHour = totalHour + partTimeHour;
 
-		        	 dailyWage = dailyWage+partTimeHour * wagePerHour;
-		             System.out.println("Employee is present half time" +dailyWage);
+		        	 dailyWage = dailyWage+partTimeHour * WAGEPerHour;
+		             System.out.println("Employee is present half time :" +dailyWage);
 		             break;
 default:		            System.out.println("Employee is absent");
 		        }
 		        System.out.println("Working days is : " + totalWorkingDays   +   " Hours: "  +totalHour);
 		    }
-		        System.out.println("Employee total wage is: " + dailyWage);
+		        System.out.println("Employee total wage is: " + dailyWage  + " of +" +company );
 }
 public static void main(String[] args) {
-	calculateWage();
+	calculateWage("Tata",20,22, 100);
+	calculateWage("Mahindra",21,21,90);
+	calculateWage("Amazon",22,20, 85);
+
 		}
 }
